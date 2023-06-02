add6c33f9c8edf7cf943ed658bda6302c40e0b1a
dave97095 <120440895+dave97095@users.noreply.github.com>
2023-06-02 15:42:44 -0500

    Commit inicial

---
+++ js/main.js
@@ -0,0 +1,77 @@
+function circulo(){
+    let figura=document.getElementById("figura");
+    figura.classList= "";
+    figura.classList.toggle("circulo");
+}
+function rectangulo(){
+    let figura=document.getElementById("figura");
+    figura.classList= "";
+    figura.classList.toggle("rectangulo");
+}
+
+function triangulo(){
+    let figura=document.getElementById("figura");
+    figura.classList= "";
+    figura.classList.toggle("triangulo");
+}
+function girar(){
+    let figura=document.getElementById("figura");
+    figura.classList= "";
+    figura.classList.toggle("girar");
+}
+function arriba(){
+    let figura=document.getElementById("figura");
+    figura.classList= "";
+    figura.classList.toggle("arriba");
+}
+function abajo(){
+    let figura=document.getElementById("figura");
+    figura.classList= "";
+    figura.classList.toggle("abajo");
+}
+
+function izquierda(){
+    let figura=document.getElementById("figura");
+    figura.classList= "";
+    figura.classList.toggle("izquierda");
+}
+
+function derecha(){
+    let figura=document.getElementById("figura");
+    figura.classList= "";
+    figura.classList.toggle("derecha");
+}
+
+function ocultar(){
+    let figura=document.getElementById("figura");
+    figura.classList= "";
+    figura.classList.toggle("ocultar");
+}
+
+function gif(){
+    let figura=document.getElementById("figura");
+    figura.classList= "";
+    figura.classList.toggle("gif");
+}
+
+function degradado(){
+    let figura=document.getElementById("figura");
+    figura.classList= "";
+    figura.classList.toggle("degradado");
+}
+
+
+
+  
+function panel(){
+    const panel = document.querySelector(".panelIzq")
+    figura.classList= "";
+    panel.classList.toggle("activo");
+}  
+ 
+function panelSupe(){
+    const panel = document.querySelector(".panelSupe")
+    figura.classList= "";
+    panel.classList.toggle("activo1");
+}  
+  
